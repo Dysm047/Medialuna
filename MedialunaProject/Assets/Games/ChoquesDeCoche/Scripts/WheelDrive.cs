c0a07baaa0195d6b69d78aa0d8e6ecb5273f9c30
Dysm <d.perez@colegioarula.com>
2021-05-06 11:45:14 +0200

    Empezamos a hacer el easyMode xd

--- MedialunaProject/Assets/Games/ChoquesDeCoche/Scripts/WheelDrive.cs
+++ MedialunaProject/Assets/Games/ChoquesDeCoche/Scripts/WheelDrive.cs
@@ -34,19 +34,24 @@ public class WheelDrive : NetworkBehaviour
     private WheelCollider[] m_Wheels;
 
     // Find all the WheelColliders down in the hierarchy.
+    public Boolean hardMode = false;
+    public GameObject carBox;
 	void Start()
 	{
-		m_Wheels = GetComponentsInChildren<WheelCollider>();
-
-		for (int i = 0; i < m_Wheels.Length; ++i) 
+		if (hardMode)
 		{
-			var wheel = m_Wheels [i];
+			m_Wheels = GetComponentsInChildren<WheelCollider>();
 
-			// Create wheel shapes only when needed.
-			if (wheelShape != null)
+			for (int i = 0; i < m_Wheels.Length; ++i)
 			{
-				var ws = Instantiate (wheelShape);
-				ws.transform.parent = wheel.transform;
+				var wheel = m_Wheels[i];
+
+				// Create wheel shapes only when needed.
+				if (wheelShape != null)
+				{
+					var ws = Instantiate(wheelShape);
+					ws.transform.parent = wheel.transform;
+				}
 			}
 		}
 	}
@@ -58,55 +63,72 @@ public class WheelDrive : NetworkBehaviour
 	{
 		if (isLocalPlayer)
 		{
-			m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
-
-			float angle = maxAngle * Input.GetAxis("Horizontal");
-			float torque = maxTorque * Input.GetAxis("Vertical");
-
-			float handBrake = Input.GetKey(KeyCode.X) ? brakeTorque : 0;
-
-			foreach (WheelCollider wheel in m_Wheels)
+			if (hardMode) //Añade más tarde la configuración para cambiar entre modo dificil y fácil
 			{
-				// A simple car where front wheels steer while rear ones drive.
-				if (wheel.transform.localPosition.z > 0)
-					wheel.steerAngle = angle;
+				m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
 
-				if (wheel.transform.localPosition.z < 0)
+				float angle = maxAngle * Input.GetAxis("Horizontal");
+				float torque = maxTorque * Input.GetAxis("Vertical");
+				print(angle);
+				print(torque);
+
+				float handBrake = Input.GetKey(KeyCode.X) ? brakeTorque : 0;
+
+				foreach (WheelCollider wheel in m_Wheels)
 				{
-					wheel.brakeTorque = handBrake;
-				}
+					// A simple car where front wheels steer while rear ones drive.
+					if (wheel.transform.localPosition.z > 0)
+						wheel.steerAngle = angle;
 
-				if (wheel.transform.localPosition.z < 0 && driveType != DriveType.FrontWheelDrive)
-				{
-					wheel.motorTorque = torque;
-				}
-
-				if (wheel.transform.localPosition.z >= 0 && driveType != DriveType.RearWheelDrive)
-				{
-					wheel.motorTorque = torque;
-				}
-
-				// Update visual wheels if any.
-				if (wheelShape)
-				{
-					Quaternion q;
-					Vector3 p;
-					wheel.GetWorldPose(out p, out q);
-
-					// Assume that the only child of the wheelcollider is the wheel shape.
-					Transform shapeTransform = wheel.transform.GetChild(0);
-
-					if (wheel.name == "a0l" || wheel.name == "a1l" || wheel.name == "a2l")
+					if (wheel.transform.localPosition.z < 0)
 					{
-						shapeTransform.rotation = q * Quaternion.Euler(0, 180, 0);
-						shapeTransform.position = p;
+						wheel.brakeTorque = handBrake;
 					}
-					else
+
+					if (wheel.transform.localPosition.z < 0 && driveType != DriveType.FrontWheelDrive)
 					{
-						shapeTransform.position = p;
-						shapeTransform.rotation = q;
+						wheel.motorTorque = torque;
+					}
+
+					if (wheel.transform.localPosition.z >= 0 && driveType != DriveType.RearWheelDrive)
+					{
+						wheel.motorTorque = torque;
+					}
+
+					// Update visual wheels if any.
+					if (wheelShape)
+					{
+						Quaternion q;
+						Vector3 p;
+						wheel.GetWorldPose(out p, out q);
+
+						// Assume that the only child of the wheelcollider is the wheel shape.
+						Transform shapeTransform = wheel.transform.GetChild(0);
+
+						if (wheel.name == "a0l" || wheel.name == "a1l" || wheel.name == "a2l")
+						{
+							shapeTransform.rotation = q * Quaternion.Euler(0, 180, 0);
+							shapeTransform.position = p;
+						}
+						else
+						{
+							shapeTransform.position = p;
+							shapeTransform.rotation = q;
+						}
 					}
 				}
+			}
+			else
+			{
+				float angle = maxAngle * Input.GetAxis("Horizontal");
+				float torque = maxTorque * Input.GetAxis("Vertical");
+				print(transform.forward);
+				Rigidbody carBoxRigidBody = GetComponent<Rigidbody>();
+				carBoxRigidBody.AddForce(transform.forward * torque);
+
+
+
+
 			}
 		}
 		if (!isLocalPlayer)
